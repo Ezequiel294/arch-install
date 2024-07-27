11eb1e311508c6ccff389e55ea4c32da85df7f77
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-07-26 22:29:44 -0600

    drivers and looks tweeks

--- base-install.sh
+++ base-install.sh
@@ -10,43 +10,41 @@ if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root" >&2
     exit 1
 fi
-echo "You are root. Proceeding with installation..."
+echo -e "\nYou are root. Proceeding with installation...\n"
 
-echo "To set the time zone, you need to know your region and city. You can find your region by running timedatectl list-timezones"
-read -p "Do you want to run 'timedatectl list-timezones'? (y/n): " answer
-if [[ $answer =~ ^[Yy]$ ]]; then
+# Set the root password, username, and hostname
+read -sp "Enter the password for root: " root_password
+read -p "Enter the username you want to create: " username
+read -sp "Enter the password for $username: " user_password
+read -p "Enter the host name: " hostname
+
+# Set timezone
+echo -e "\nTo set the time zone, you need to know your region and city." 
+echo "You can find your region and city by running timedatectl list-timezones"
+read -p "Do you want to run 'timedatectl list-timezones'? (Y/n): " answer
+if [[ $answer =~ ^[Yy\n]$ ]]; then
     timedatectl list-timezones
 fi
 read -p "Enter your region: " region
-echo
 read -p "Enter your city: " city
-echo
-read -sp "Enter the password for root: " root_password
-echo
-read -p "Enter the username you want to create: " username
-echo
-read -sp "Enter the password for $username: " user_password
-echo
-read -p "Enter the host name: " hostname
-echo
-
-# Set the time zone
-echo "Setting the time zone..."
+echo -e "\nSetting the time zone..."
 ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
 hwclock --systohc
 echo "Time zone set."
 
 # Set accounts
-echo "Setting up accounts..."
+echo -e "\nSetting up accounts..."
 echo "root:$root_password" | chpasswd
 useradd -mG wheel "$username"
 echo "$username:$user_password" | chpasswd
 echo "Accounts set."
 
+# Install necessary packages
+echo -e "\nInstalling necessary packages..."
 pacman -S --noconfirm neovim reflector
 
 # Pacman configuration
-echo "Configuring pacman..."
+echo -e "\nConfiguring pacman..."
 sed -i '/#Color/s/^#//' /etc/pacman.conf
 sed -i '/#ParallelDownloads/s/^#//' /etc/pacman.conf
 if ! grep -q '^ILoveCandy' /etc/pacman.conf; then
@@ -58,7 +56,7 @@ pacman -Syy
 echo "Pacman configured."
 
 # Hardware detection and conditional package installation
-echo "Detecting hardware..."
+echo -e "\nDetecting hardware..."
 is_virtualbox=$(lspci | grep -i "VirtualBox" || echo "not found")
 if [ "$is_virtualbox" != "not found" ]; then
     echo "VirtualBox environment detected. Installing VirtualBox Guest Additions..."
@@ -70,10 +68,10 @@ else
     for gpu_info in $(lspci | grep -E "VGA|3D|2D"); do
         if echo "$gpu_info" | grep -iq "nvidia"; then
             echo "NVIDIA GPU detected. Installing drivers..."
-            pacman -S --needed --noconfirm nvidia nvidia-utils nvidia-settings
+            pacman -S --needed --noconfirm nvidia nvidia-utils nvidia-settings mesa
         elif echo "$gpu_info" | grep -iq "amd"; then
             echo "AMD GPU detected. Installing drivers..."
-            pacman -S --needed --noconfirm xf86-video-amdgpu vulkan-radeon
+            pacman -S --needed --noconfirm mesa libva-mesa-driver xf86-video-amdgpu vulkan-radeon
         elif echo "$gpu_info" | grep -iq "intel"; then
             echo "Intel GPU detected. Installing drivers..."
             pacman -S --needed --noconfirm mesa vulkan-intel
@@ -97,48 +95,51 @@ fi
 echo "Hardware detected and set up."
 
 # Install necessary packages
-echo "Installing necessary packages..."
+echo -e "\nInstalling necessary packages..."
 pacman -S --needed --noconfirm base-devel grub efibootmgr networkmanager pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack bluez bluez-utils sudo
 echo "Packages installed."
 
 # Enable sudo
-echo "Enabling sudo..."
+echo -e "\nEnabling sudo..."
 sed -i '/^# %wheel ALL=(ALL:ALL) ALL/s/^# //' /etc/sudoers
 echo "Sudo enabled."
 
 # Set the locale
-echo "Setting the locale..."
+echo -e "\nSetting the locale..."
 sed -i '/^#en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LANG=en_US.UTF-8" >/etc/locale.conf
 echo "Locale set."
 
 # Grub installation
-echo "Installing GRUB..."
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "\nInstalling GRUB..."
+grub-install --verbose --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 echo "GRUB installed."
+echo -e "\nGenerating GRUB configuration..."
+grub-mkconfig -o /boot/grub/grub.cfg
+echo "GRUB configuration generated."
 
 # Network configuration
-echo "Configuring network..."
-echo $hostname > /etc/hostname
+echo -e "\nConfiguring network..."
+echo $hostname >/etc/hostname
 echo "127.0.0.1 localhost
 ::1       localhost
-127.0.1.1 $hostname.localhost $hostname" | tee /etc/hosts > /dev/null
+127.0.1.1 $hostname.localhost $hostname" | tee /etc/hosts >/dev/null
 echo "Network configured."
 
 # Enable services
-echo "Enabling services..."
+echo -e "\nEnabling services..."
 systemctl enable NetworkManager bluetooth
 echo "Services enabled."
 
-
 # Clone my dotfiles
-echo "Moving to $username's home directory..."
+echo -e "\nMoving to $username's home directory..."
 cd /home/$username
-echo "Installing dotfiles..."
+echo -e "\nInstalling dotfiles..."
 git clone --bare https://github.com/Ezequiel294/dotfiles .dotfiles
 git --git-dir /home/$username/.dotfiles/ --work-tree /home/$username checkout --force
 echo "Installation complete."
 
-echo "Reboot and run the post-install script in your home directory."
+echo -e "\nReboot and run the post-install script in your home directory."
+echo "The script is located at /home/$username/Scripts/Arch/post-install.sh"
+
